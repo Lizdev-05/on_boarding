43a7405db50f3828d228b093dc4c5bdbea8a3405
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-17 01:44:35 +0000

    Add the list vertical dynamically

---
+++ lib/tidy_list_data.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class FeedItem {
+  final String title;
+  final String description;
+  final String subtitle;
+  final Color? color;
+
+  FeedItem(
+      {required this.title,
+      required this.description,
+      required this.subtitle,
+      required this.color});
+}
+
+class TidyListData extends StatelessWidget {
+  TidyListData({super.key});
+
+  List<FeedItem> feedItems = [
+    FeedItem(
+        title: "Window Cleaning",
+        description:
+            "Clean window and window \n frame of various types using \n high quality leaning agents",
+        // ignore: unnecessary_string_escapes
+        subtitle: "From 100",
+        color: Color.fromARGB(255, 168, 212, 230)),
+    FeedItem(
+        title: "Carpet Dry Cleaning",
+        description:
+            "Professionally clean carpet\n using a dry cleaning method \n to remove dirt and stains",
+        subtitle: "From 500",
+        color: Color.fromARGB(255, 233, 199, 177)),
+    FeedItem(
+        title: "Furniture Dry Cleaning",
+        description:
+            "Professionally clean furniture \n using a dry cleaning method \n to remove dirt and stains",
+        subtitle: "From 150",
+        color: Color.fromARGB(255, 213, 197, 255)),
+    FeedItem(
+        title: "Window Cleaning",
+        description:
+            "Clean window and window \n frame of various types using \n high quality leaning agents",
+        // ignore: unnecessary_string_escapes
+        subtitle: "From 100",
+        color: const Color.fromARGB(255, 177, 220, 255)),
+    FeedItem(
+        title: "Carpet Dry Cleaning",
+        description:
+            "Professionally clean carpet \n using a dry cleaning method \n to remove dirt and stains",
+        subtitle: "From 500",
+        color: Color.fromARGB(255, 213, 197, 255)),
+    FeedItem(
+        title: "Window Cleaning",
+        description:
+            "Clean window and window \n frame of various types using \n high quality leaning agents",
+        // ignore: unnecessary_string_escapes
+        subtitle: "From 100",
+        color: Color.fromARGB(255, 233, 199, 177)),
+    FeedItem(
+        title: "Furniture Dry Cleaning",
+        description:
+            "Professionally clean furniture \n using a dry cleaning method \n to remove dirt and stains",
+        subtitle: "From 150",
+        color: Color.fromARGB(255, 168, 212, 230)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: feedItems.length,
+      itemBuilder: (context, index) {
+        FeedItem item = feedItems[index];
+        return Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Card(
+            color: item.color ?? Colors.white,
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        item.title,
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(width: 70),
+                      Text(
+                        item.subtitle,
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w700),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        item.description,
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: OutlinedButton.icon(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 31, 30, 30),
+                              foregroundColor: Colors.white
+                              // textStyle: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                              ),
+                          icon: const Icon(Icons.arrow_right_alt),
+                          label: const Text("Book"),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/tidy_me_page.dart
+++ lib/tidy_me_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:on_bord/tidy_list_data.dart';
 
 class TidyMePage extends StatefulWidget {
   const TidyMePage({super.key});
@@ -15,17 +16,104 @@ class _TidyMePageState extends State<TidyMePage> {
       appBar: AppBar(
         actions: const [
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
             child: Icon(
               Icons.alarm,
-              color: Colors.grey,
+              color: Color.fromARGB(255, 244, 236, 236),
             ),
           ),
         ],
         backgroundColor: const Color.fromARGB(255, 37, 36, 36),
         title: const Text(
           "Home",
-          style: TextStyle(color: Colors.grey),
+          style: TextStyle(color: Color.fromARGB(255, 244, 236, 236)),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            ListTile(
+              leading: const Text(
+                "Our Offers",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              trailing: TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                  size: 28,
+                ), // Place the icon first
+                label: const Text("View All",
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      const Row(
+                        children: [
+                          Text(
+                            "Get 5% off",
+                            style: TextStyle(color: Colors.black, fontSize: 38),
+                          ),
+                          SizedBox(width: 100),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.adb_rounded,
+                                color: Colors.black,
+                                size: 28,
+                              ),
+                              Icon(
+                                Icons.adb_rounded,
+                                color: Colors.black,
+                                size: 28,
+                              ),
+                              Icon(
+                                Icons.adb_rounded,
+                                color: Colors.black,
+                                size: 28,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Text(
+                            "Use cleaning service \n on a regular basis",
+                            style: TextStyle(color: Colors.black, fontSize: 18),
+                          ),
+                          const SizedBox(width: 20),
+                          Expanded(
+                            child: OutlinedButton.icon(
+                              onPressed: () {},
+                              style: OutlinedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 31, 30, 30),
+                                  foregroundColor: Colors.white
+                                  // textStyle: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                                  ),
+                              icon: const Icon(Icons.arrow_right_alt),
+                              label: const Text("Check it out"),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TidyListData(),
+            )
+          ],
         ),
       ),
     );
